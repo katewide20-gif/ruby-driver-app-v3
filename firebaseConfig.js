7c006fb8757ce9ec79253011bf4ec377a9de99c6
katewide20-gif <katewide20@gmail.com>
2026-09-16 16:24:27 +0100

    Fix API key formatting and export statements

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXJOwGuQ4UVFHh229OMQIQEXyUhv59T-E",
+  apiKey: "AIzaSyDXJ0wGuQ4UVFHh2290MQIQEXyUhv59T-E",
   authDomain: "ruby-54b40.firebaseapp.com",
   projectId: "ruby-54b40",
   storageBucket: "ruby-54b40.firebasestorage.app",
@@ -11,6 +11,6 @@ const firebaseConfig = {
   appId: "1:539058257239:web:97b13446f83304c6119212"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
